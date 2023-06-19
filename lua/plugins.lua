ed0fe9b299000fbd29cbdf49bcc708461a35bdc9
Your name <Your email>
2023-06-19 19:57:15 -0300

    atajos y correciones color

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,5 +3,5 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use { "bluz71/vim-nightfly-colors", as = "nightfly" }
-
+  use("christoomey/vim-tmux-navigator")
 end)
