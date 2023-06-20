3465cbfe715d749d5056b07b4789ba66f0fe929d
Your name <Your email>
2023-06-19 21:16:33 -0300

    nvimtree ffinder

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,15 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use("nvim-lua/plenary.nvim")
   use { "bluz71/vim-nightfly-colors", as = "nightfly" }
   use("christoomey/vim-tmux-navigator")
+  use("tpope/vim-surround")
+  use("inkarkat/vim-ReplaceWithRegister")
+  use("numToStr/Comment.nvim")
+  use("nvim-tree/nvim-tree.lua")
+  use("nvim-tree/nvim-web-devicons")
+  use("nvim-lualine/lualine.nvim")
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 end)
